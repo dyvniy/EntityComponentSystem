c7d441dc84dc06192b2ccdd5df3414fd8225a542
Nick Vovk <dyvniy@yandex.ru>
2021-12-26 20:40:22 +0300

    add console project for test, fix dependences for x64 release & debug

---
+++ ConsoleApplicationRun/ConsoleApplicationRun.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+#include "../EntityComponentSystem/include/ECS/API.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    ECS::Memory::GlobalMemoryUser gmu;
+    std::cout << "class from dll created\n";
+
+    char* str10 = (char*)gmu.Allocate(10);
+    
+    for (int i = 0; i < 10; ++i) str10[i] = '9';
+    str10[9] = 0; // without if in Debug print too long string
+    std::cout << "Print from allocated: " << str10 << "\n";
+    
+    gmu.Free((void*)str10);
+    std::cout << "memory free";
+}
+
---
+++ testEntityComponentSystem/testMain.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+
+#include <iostream>
+
+using namespace std;
+
+int WinMain() 
+{
+	cout << "Hello from test!\n";
+	return 0;
+}
+
+int main()
+{
+	cout << "Hello from test!\n";
+	return 0;
+}
